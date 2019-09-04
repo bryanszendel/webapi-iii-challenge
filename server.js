331c2380ca297ddd5a7df34279b6a30bc9953169
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-04 13:45:01 -0600

    GET and POST to /users via userRouter are working

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const express = 'express';
+const express = require('express');
+const path = require('path')
+
+const postRouter = require('./posts/postRouter')
+const userRouter = require('./users/userRouter')
 
 const server = express();
+server.use(express.json())
+
+server.use('/posts', postRouter)
+server.use('/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,21 @@
-const express = 'express';
+const express = require('express');
+const users = require('./userDb')
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-
+  const newUser = req.body
+  users.insert(newUser)
+    .then(result => {
+      if (!newUser.name) {
+        res.status(400).json({ message: "Please include a name for the user"})
+      } else {
+        res.status(201).json(result)
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "There was an error creating the user"})
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,7 +23,13 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+  users.get()
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Error retrieving the users"})
+    })
 });
 
 router.get('/:id', (req, res) => {
